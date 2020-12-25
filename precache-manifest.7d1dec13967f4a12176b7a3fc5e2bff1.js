b48d2f2c319fa8642e570b87149dbc6a7d48e224
Jason Witt <jawittdesigns@gmail.com>
2020-12-25 10:18:36 -0500

    Updates

---
+++ precache-manifest.7d1dec13967f4a12176b7a3fc5e2bff1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4eecfb5787ba2ccca3763a4d0253fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "539bbd0ec10f252c4999",
+    "url": "./static/css/2.2f475074.chunk.css"
+  },
+  {
+    "revision": "c4733c345dd8a2f46c9e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "539bbd0ec10f252c4999",
+    "url": "./static/js/2.750e530d.chunk.js"
+  },
+  {
+    "revision": "7d868ef600d7dc3328989dedddb23e90",
+    "url": "./static/js/2.750e530d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4733c345dd8a2f46c9e",
+    "url": "./static/js/main.ccf5c024.chunk.js"
+  },
+  {
+    "revision": "cf2c17ad6d7c08908f15",
+    "url": "./static/js/runtime-main.d0faa964.js"
+  },
+  {
+    "revision": "6a47133378275f52f65cb1b0daa350cd",
+    "url": "./static/media/fira-code-all-400-normal.6a471333.woff"
+  },
+  {
+    "revision": "50f767c5e2e184d2c58a510e8e7e3b64",
+    "url": "./static/media/fira-code-cyrillic-400-normal.50f767c5.woff2"
+  },
+  {
+    "revision": "d32a2f96078d9b549d75599f1097ba71",
+    "url": "./static/media/fira-code-cyrillic-ext-400-normal.d32a2f96.woff2"
+  },
+  {
+    "revision": "bb28833a1b3ee46c90cf05d3728d1d83",
+    "url": "./static/media/fira-code-greek-400-normal.bb28833a.woff2"
+  },
+  {
+    "revision": "eafc069988f6fd7d0385746c962122af",
+    "url": "./static/media/fira-code-greek-ext-400-normal.eafc0699.woff2"
+  },
+  {
+    "revision": "c6f7f2bfaccffae1d24d6945b822791e",
+    "url": "./static/media/fira-code-latin-400-normal.c6f7f2bf.woff2"
+  },
+  {
+    "revision": "03c248ada875e694ddd54e29afdcfce3",
+    "url": "./static/media/fira-code-latin-ext-400-normal.03c248ad.woff2"
+  }
+]);
